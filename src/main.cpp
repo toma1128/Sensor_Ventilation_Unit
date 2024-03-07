cdaead4beb661f51d59bd95532cf1291f1e94dad
2230062 <2230062@ecc.ac.jp>
2024-03-07 12:26:44 +0900

    テスト1,とりあえず書き込めた

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,17 @@ Servo servo1;
 float ad_val; // アナログ入力値
 int angle;    //動かす度数
 
-int sensPin = 2;   //センサーのピン
-int sensVal;
-int crtVal;
+const int SENS_PIN = 13;  //センサーのピン
+int sensVal = 0;
+int crtVal = 0;       //前回の結果を記録しておく変数
+
+int count = 0;
 
 // 初期設定 -----------------------------------------
 void setup() {
   Serial.begin(115200);
   pinMode(32, ANALOG);          //アナログ入力端子設定
+  pinMode(SENS_PIN, INPUT_PULLUP);
 
   // PWM設定（以下のタイマ1つで4チャンネル使用可のため、最大16台の設定が可能）
 	ESP32PWM::allocateTimer(0); // ledcチャンネル 0,1,8,9用タイマ(C Plusではノイズ音が鳴るため非推奨)
@@ -29,35 +32,27 @@ void setup() {
 
 // メイン -------------------------------------------
 void loop() {
-  angle = 60;               // 角度指定
-  servo1.write(angle);      // 角度を指定してサーボ動作実行（「最小角度Dutyパルス幅時間μs」以下の場合は角度として扱われ、それ以外はパルス幅μsとして扱われます。）
+  sensVal = digitalRead(SENS_PIN); //センサー読み取り
 
-  delay(1000);
-
-  ad_val = analogRead(32);  // アナログ入力値（0〜4095）を取得
-    angle = map(ad_val, 0, 4095, 0, 180);  // アナログ値 （0〜4095）→ 角度（0゜〜180゜）換算
-    servo1.write(angle);      // 角度を指定してサーボ動作実行（「最小角度Dutyパルス幅時間μs」以下の場合は角度として扱われ、それ以外はパルス幅μsとして扱われます。）
-
-
-
-  sensVal = digitalRead(sensPin); //センサー読み取り
   if(crtVal != sensVal){
     Serial.print("Sensor Value = ");
-    Serial.println(sensPin,BIN);
+    Serial.println(sensVal,BIN);
+    if(sensVal == 1){
 
-    angle = 60;               // 角度指定
-    servo1.write(angle);      // 角度を指定してサーボ動作実行（「最小角度Dutyパルス幅時間μs」以下の場合は角度として扱われ、それ以外はパルス幅μsとして扱われます。）
+      angle = 90;               // 角度指定
+      servo1.write(angle);      // 角度を指定してサーボ動作実行
 
-  delay(1000);
+      delay(1000);
 
-  ad_val = analogRead(32);  // アナログ入力値（0〜4095）を取得
-    angle = map(ad_val, 0, 4095, 0, 180);  // アナログ値 （0〜4095）→ 角度（0゜〜180゜）換算
-    servo1.write(angle);
-
+      angle = 0;
+      servo1.write(angle);
+    }else{
+      Serial.println("停止");
+    }
     crtVal = sensVal;
   }
+  count++;
+  Serial.println(count);
 
-  // シリアルモニタ出力（角度、Dutyパルス幅時間μs）※使用する場合は有効にしてdelay時間長くする。
-  // Serial.printf("Angle : %d'  Pulse Width(us) : %d\n", servo1.read(), servo1.readMicroseconds());
   delay(1000); // 遅延時間
 }
